2e7635d2239de19d7639fe410ae770ef4ca7d32a
Fabio Tiriticco <fabio.europe@gmail.com>
2015-12-27 14:18:06 +0100

    removed private defs + typos and code style

--- src/main/scala/io/ticofab/AwsSigner.scala
+++ src/main/scala/io/ticofab/AwsSigner.scala
@@ -42,7 +42,7 @@ case class AwsSigner(credentialsProvider: AWSCredentialsProvider,
   val SLASH = "/"
   val X_AMZ_DATE = "x-amz-date"
   val RETURN = "\n"
-  val AWS4_HMAC_SHA256 = "AWS4-HMAC-SHA256\n"
+  val AWS4_HMAC_SHA256 = "AWS4-HMAC-SHA256"
   val AWS4_REQUEST = "/aws4_request"
   val AWS4_HMAC_SHA256_CREDENTIAL = "AWS4-HMAC-SHA256 Credential="
   val SIGNED_HEADERS = ", SignedHeaders="
@@ -65,7 +65,67 @@ case class AwsSigner(credentialsProvider: AWSCredentialsProvider,
                        queryParams: Map[String, String],
                        headers: Map[String, String],
                        payload: Option[Array[Byte]]): Map[String, String] = {
+
+    def queryParamsString(queryParams: Map[String, String]) =
+      queryParams.map(pair => pair._1 + "=" + URLEncoder.encode(pair._2, "UTF-8")).mkString("&")
+
+    def sign(stringToSign: String, now: DateTime, credentials: AWSCredentials): String = {
+      def hmacSHA256(data: String, key: Array[Byte]): Array[Byte] = {
+        try {
+          val mac: Mac = Mac.getInstance(HMAC_SHA256)
+          mac.init(new SecretKeySpec(key, HMAC_SHA256))
+          mac.doFinal(data.getBytes(StandardCharsets.UTF_8))
+        } catch {
+          case e: NoSuchAlgorithmException => throw e
+          case i: InvalidKeyException => throw i
+        }
+      }
+
+      def getSignatureKey(now: DateTime, credentials: AWSCredentials): Array[Byte] = {
+        val kSecret: Array[Byte] = (AWS4 + credentials.getAWSSecretKey).getBytes(StandardCharsets.UTF_8)
+        val kDate: Array[Byte] = hmacSHA256(now.toString(ISODateTimeFormat.basicDate()), kSecret)
+        val kRegion: Array[Byte] = hmacSHA256(region, kDate)
+        val kService: Array[Byte] = hmacSHA256(service, kRegion)
+        hmacSHA256(AWS_4_REQUEST, kService)
+      }
+
+      Hex.encodeHexString(hmacSHA256(stringToSign, getSignatureKey(now, credentials)))
+    }
+
+    def headerAsString(header: (String, Object)): String =
+      if (header._1.equalsIgnoreCase(CONNECTION)) {
+        CONNECTION + CLOSE
+      } else if (header._1.equalsIgnoreCase(CONTENT_LENGTH) && header._2.equals(ZERO)) {
+        header._1.toLowerCase() + ':'
+      } else {
+        header._1.toLowerCase() + ':' + header._2
+      }
+
+    def getCredentialScope(now: DateTime): String =
+      now.toString(ISODateTimeFormat.basicDate()) + SLASH + region + SLASH + service + AWS4_REQUEST
+
+    def hash(payload: Array[Byte]): Array[Byte] =
+      try {
+        val md: MessageDigest = MessageDigest.getInstance(SHA_256)
+        md.update(payload)
+        md.digest
+      } catch {
+        case n: NoSuchAlgorithmException => throw n
+      }
+
+    def toBase16(data: Array[Byte]): String =
+      data.map(byte => (BASE16MAP(byte >> 4 & 0xF), BASE16MAP(byte & 0xF))).toList.flatMap(pair => List(pair._1, pair._2)).mkString
+
+    def createStringToSign(canonicalRequest: String, now: DateTime): String =
+      AWS4_HMAC_SHA256 + RETURN +
+        now.toString(DATE_FORMATTER) + RETURN +
+        getCredentialScope(now) + RETURN +
+        toBase16(hash(canonicalRequest.getBytes(StandardCharsets.UTF_8)))
+
+    // evaluate current time from the provided clock
     val now: DateTime = clock.apply()
+
+    // signing credentials
     val credentials: AWSCredentials = credentialsProvider.getCredentials
 
     var result = TreeMap[String, String]()(Ordering.by(_.toLowerCase))
@@ -85,7 +145,8 @@ case class AwsSigner(credentialsProvider: AWSCredentialsProvider,
     val signedHeaders: List[String] = result.toMap.map(pair => pair._1.toLowerCase).toList
 
     val signedHeaderKeys = signedHeaders.mkString(";")
-    val canonicalRequest = method + RETURN +
+    val canonicalRequest =
+      method + RETURN +
       uri + RETURN +
       queryParamsString(queryParams) + RETURN +
       headersString + RETURN +
@@ -94,75 +155,13 @@ case class AwsSigner(credentialsProvider: AWSCredentialsProvider,
 
     val stringToSign = createStringToSign(canonicalRequest, now)
     val signature = sign(stringToSign, now, credentials)
-    val autorizationHeader = AWS4_HMAC_SHA256_CREDENTIAL +
+    val authorizationHeader = AWS4_HMAC_SHA256_CREDENTIAL +
       credentials.getAWSAccessKeyId + SLASH + getCredentialScope(now) +
       SIGNED_HEADERS + signedHeaderKeys +
       SIGNATURE + signature
 
-    result += (AUTHORIZATION -> autorizationHeader)
+    result += (AUTHORIZATION -> authorizationHeader)
 
     result.toMap
   }
-
-  private def queryParamsString(queryParams: Map[String, String]) =
-    queryParams.map(pair => pair._1 + "=" + URLEncoder.encode(pair._2, "UTF-8")).mkString("&")
-
-  private def headerAsString(header: (String, Object)): String = {
-    if (header._1.equalsIgnoreCase(CONNECTION)) {
-      CONNECTION + CLOSE
-    } else if (header._1.equalsIgnoreCase(CONTENT_LENGTH) && header._2.equals(ZERO)) {
-      header._1.toLowerCase() + ':'
-    } else {
-      header._1.toLowerCase() + ':' + header._2
-    }
-  }
-
-  private def sign(stringToSign: String, now: DateTime, credentials: AWSCredentials): String = {
-    Hex.encodeHexString(hmacSHA256(stringToSign, getSignatureKey(now, credentials)))
-  }
-
-  private def createStringToSign(canonicalRequest: String, now: DateTime): String = {
-    AWS4_HMAC_SHA256 +
-      now.toString(DATE_FORMATTER) + RETURN +
-      getCredentialScope(now) + RETURN +
-      toBase16(hash(canonicalRequest.getBytes(StandardCharsets.UTF_8)))
-  }
-
-  private def getCredentialScope(now: DateTime): String = {
-    now.toString(ISODateTimeFormat.basicDate()) + SLASH + region + SLASH + service + AWS4_REQUEST
-  }
-
-  private def hash(payload: Array[Byte]): Array[Byte] = {
-    try {
-      val md: MessageDigest = MessageDigest.getInstance(SHA_256)
-      md.update(payload)
-      md.digest
-    } catch {
-      case n: NoSuchAlgorithmException => throw n
-    }
-  }
-
-  private def toBase16(data: Array[Byte]): String = {
-    data.map(byte => (BASE16MAP(byte >> 4 & 0xF), BASE16MAP(byte & 0xF))).toList.flatMap(pair => List(pair._1, pair._2)).mkString
-  }
-
-  private def getSignatureKey(now: DateTime, credentials: AWSCredentials): Array[Byte] = {
-    val kSecret: Array[Byte] = (AWS4 + credentials.getAWSSecretKey).getBytes(StandardCharsets.UTF_8)
-    val kDate: Array[Byte] = hmacSHA256(now.toString(ISODateTimeFormat.basicDate()), kSecret)
-    val kRegion: Array[Byte] = hmacSHA256(region, kDate)
-    val kService: Array[Byte] = hmacSHA256(service, kRegion)
-    hmacSHA256(AWS_4_REQUEST, kService)
-  }
-
-  private def hmacSHA256(data: String, key: Array[Byte]): Array[Byte] = {
-    try {
-      val mac: Mac = Mac.getInstance(HMAC_SHA256)
-      mac.init(new SecretKeySpec(key, HMAC_SHA256))
-      mac.doFinal(data.getBytes(StandardCharsets.UTF_8))
-    } catch {
-      case e: NoSuchAlgorithmException => throw e
-      case i: InvalidKeyException => throw i
-    }
-  }
-
 }
